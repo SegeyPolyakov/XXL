4105e320c54666a328d5a205bd01490972da29a2
SegeyPolyakov <cfvcfvsx69@mail.ru>
2024-08-31 16:22:39 +0300

    Add files via upload

---
+++ massiv_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+int main()
+{
+    int numbers[] = {1, 2, 3, 4};   
+    numbers[0] = numbers[0] * 2;  
+    numbers[3] = numbers[3] * 2;  
+    for(int i = 0; i < 4; i++)
+    {
+        printf("%d", numbers[i]);
+    }
+    return 0;
+}
