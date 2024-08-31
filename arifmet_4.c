30d93b47af062d2c1a27d23cfc3de173ee205451
SegeyPolyakov <cfvcfvsx69@mail.ru>
2024-08-31 13:19:38 +0300

    Add files via upload

---
+++ arifmet_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+  int a = 10;
+  int b = 5;
+  int sum = a + b;           
+  float average = sum / 2.0; 
+  printf("Sum: %d\n", sum);
+  printf("Average: %f\n", average);
+ return 0;
+}
